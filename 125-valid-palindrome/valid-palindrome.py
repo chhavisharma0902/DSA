d344dce39302331da494b117aede599d957e459c
Chhavi Vishnu Sharma <204506444+chhavisharma0902@users.noreply.github.com>
2026-07-14 01:15:15 +0530

    Time: 8 ms (95.54%) | Memory: 13.6 MB (28.04%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = s.lower()
+        s = "".join([char for char in s if char.isalnum()])
+        
+        left = 0
+        right = len(s)-1
+        while(left<right):
+            if(s[left]!=s[right]):
+                return False
+            left+=1
+            right-=1
+        return True
