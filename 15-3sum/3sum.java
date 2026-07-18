48059b63b7bac26351d2587780084255a4acd830
Chhavi Vishnu Sharma <204506444+chhavisharma0902@users.noreply.github.com>
2026-07-18 15:39:00 +0530

    Time: 28 ms (97.07%) | Memory: 59.2 MB (60.12%) - LeetSync

---
+++ 15-3sum/3sum.java
@@ -0,0 +1,43 @@
+class Solution {
+    public void threeSumHelper(int f , int[] nums, List<List<Integer>> res){
+        int i = f + 1;
+        int j = nums.length - 1;
+        while(i<j){
+            int sum = nums[f] + nums[i] + nums[j];
+            if(sum > 0){
+                j = j - 1;
+            }
+            else if(sum < 0){
+                i = i + 1;
+            }
+            else{
+                res.add(Arrays.asList(nums[f],nums[i],nums[j]));
+                i = i + 1;
+                j = j - 1;
+
+                while(i<j && nums[i] == nums[i - 1]){
+                    i = i + 1;
+                }
+                while(i<j && nums[j] == nums[j + 1]){
+                    j = j - 1;
+                }
+            }
+        }
+    }
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for(int f = 0 ; f < nums.length ; f++){
+            if(nums[f] > 0){
+                break;
+            }
+
+            if(f == 0 || nums[f] != nums[f-1]){
+                threeSumHelper(f,nums,res);
+            }
+        }
+        return res;
+        
+    }
+    
+}
