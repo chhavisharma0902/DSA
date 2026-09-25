58816966a591f95a95238345f297c70cdf66d2c1
Chhavi Vishnu Sharma <204506444+chhavisharma0902@users.noreply.github.com>
2026-09-25 16:50:49 +0530

    Time: 1 ms (70.17%) | Memory: 12.6 MB (52.44%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -5,14 +5,29 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
-        merged = nums1 + nums2
-        merged.sort()
-        n = len(merged)
-        mid = n // 2
-        if n%2==0:
-            return (merged[mid]+merged[mid-1])/2.0
-        else:
-            return float(merged[mid])
+        m = len(nums1)
+        n = len(nums2)
+        total_len = m+n
+        mid = total_len//2
+        curr_val = 0
+        prev_val = 0
+        i = 0
+        j = 0
 
+        for _ in range(mid+1):
+            prev_val = curr_val
+
+            if i<m and (j>=n or nums1[i] <= nums2[j]):
+                curr_val = nums1[i]
+                i +=1
+            else:
+                curr_val = nums2[j]
+                j +=1
+            
+        if(total_len % 2==0):
+            return (prev_val + curr_val) / 2.0
+        else:
+            return float(curr_val)
+        
 
         
